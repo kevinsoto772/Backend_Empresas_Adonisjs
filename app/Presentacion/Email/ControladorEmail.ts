99b05483cd7b8b18e2c6d71a67763e36fcd17cfb
kevinsoto772 <kosotosimanca@gmail.com>
2022-12-22 16:48:01 -0500

    Login Novafianza

--- app/Presentacion/Email/ControladorEmail.ts
+++ app/Presentacion/Email/ControladorEmail.ts
@@ -3,16 +3,18 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { ServicioEmail } from 'App/Dominio/Datos/Servicios/ServicioEmail'
 import { ServicioUsuarioEmpresa } from 'App/Dominio/Datos/Servicios/ServicioUsuarioEmpresa'
+import { ServicioUsuarioNovafianza } from 'App/Dominio/Datos/Servicios/ServicioUsuarioNovafianza'
 import { GenerarContrasena } from 'App/Dominio/GenerarContrasena/GenerarContrasena'
 import { EnviadorEmailAdonis } from 'App/Infraestructura/Email/EnviadorEmailAdonis'
 import { EncriptadorAdonis } from 'App/Infraestructura/Encriptacion/EncriptadorAdonis'
 import { RepositorioUsuarioEmpresaDB } from 'App/Infraestructura/Implementacion/BaseDatos/RepositorioUsuarioEmpresaDB'
+import { RepositorioUsuarioNovafianzaDB } from '../../Infraestructura/Implementacion/BaseDatos/RepositorioUsuarioNovafianzaDB'
 
 export default class ControladorEmpresa {
   private service: ServicioEmail
   constructor () {
     this.service = new ServicioEmail (new EnviadorEmailAdonis(), new ServicioUsuarioEmpresa(new RepositorioUsuarioEmpresaDB()
-      , new GenerarContrasena(), new EncriptadorAdonis()), new GenerarContrasena())
+      , new GenerarContrasena(), new EncriptadorAdonis()), new GenerarContrasena(), new ServicioUsuarioNovafianza(new RepositorioUsuarioNovafianzaDB(),new GenerarContrasena(), new EncriptadorAdonis()))
   }
 
   public EnviarEmail ({request, response}:HttpContextContract) {
