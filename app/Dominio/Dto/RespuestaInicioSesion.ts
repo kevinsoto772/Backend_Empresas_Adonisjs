9709bbc216a53e25206f86e43f4e00aeefb0c374
Jesid Polo <jesing482@gmail.com>
2023-01-25 15:23:29 -0500

    actualizando respuesta de inicio de sesion

--- app/Dominio/Dto/RespuestaInicioSesion.ts
+++ app/Dominio/Dto/RespuestaInicioSesion.ts
@@ -2,19 +2,18 @@
 import { Rol } from '../Datos/Entidades/Autenticacion/Rol'
 
 export class RespuestaInicioSesion {
-  id: string
-  usuario: string
-  token: string
-  rol: Rol
-  nombre: string
-  claveTemporal: boolean
 
-  constructor (id: string, usuario: string, token: string, rol: Rol, nombre: string, claveTemporal: boolean) {
-    this.id = id
-    this.usuario = usuario
-    this.token = token
-    this.rol = rol
-    this.nombre = nombre
-    this.claveTemporal = claveTemporal
-  }
+  constructor(
+    public readonly usuario: {
+      id: string,
+      usuario: string,
+      nombre?: string,
+      apellido?: string,
+      telefono?: string,
+      correo?: string
+    },
+    public readonly token: string,
+    public readonly rol: Rol,
+    public readonly claveTemporal: boolean
+  ) { }
 }
