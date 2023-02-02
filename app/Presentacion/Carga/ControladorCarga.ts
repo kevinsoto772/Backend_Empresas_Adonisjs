86f934bc50dd6898853d6aeb4f3e0fbb9f3935ce
Julio Jimenez <zaor.julio@gmail.com>
2023-02-02 08:35:22 -0500

    Validación de formatos en la carga de archivos

--- app/Presentacion/Carga/ControladorCarga.ts
+++ app/Presentacion/Carga/ControladorCarga.ts
@@ -9,6 +9,7 @@ export default class ControladorCarga {
 
   public async cargar ({ request, response }) {
     try {
+      const formatos:string[] = ['txt', 'csv']
       const  datos  = request.all();
       const archivo = request.file('archivo', {
         extnames: ['txt', 'csv'],
@@ -18,10 +19,11 @@ export default class ControladorCarga {
       }
 
       if (!archivo.isValid) {
-        return response.status(400).send({mensaje:'Formato incorrecto para el archivo'})
+        return response.status(415).send({mensaje:`Formato inválido: no se puede cargar el archivo seleccionado. Inténtalo nuevamnte, los tipos de archivos permitidos son ${formatos}`})
       }
 
-      return this.servicio.procesarArchivo(archivo, JSON.stringify(datos))
+      return response.status(202).send({mensaje:'El archivo se esta procesado, puede consultar el resumen en el historial de carga'})
+      this.servicio.procesarArchivo(archivo, JSON.stringify(datos))
     } catch (error) {
       return error
     }
