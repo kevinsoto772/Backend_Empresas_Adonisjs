c8cea22cf86acafe2611362a5b0e86fc91d3cac8
CésarM <calfonsomora@gmail.com>
2023-02-06 09:05:55 -0500

    Se agregaron roles según la documentación

--- app/Dominio/DiccionarioAutorizacion.ts
+++ app/Dominio/DiccionarioAutorizacion.ts
@@ -1,14 +1,14 @@
 export const ROLES = {
     SUPER: '001',
-    USUARIO: '002'
-}
-
-export const MODULOS = {
-    ARCHIVOS: '001',
     ADMIN_NOVAFIANZA: '002',
     ADMIN_CLIENTE: '003',
     USUARIO_OPERATIVO: '004',
     CONSULTOR: '005'
+}
+
+export const MODULOS = {
+    ARCHIVOS: '001',
+    USUARIOS: '002'
 }
 
 export const FUNCIONALIDADES = {
