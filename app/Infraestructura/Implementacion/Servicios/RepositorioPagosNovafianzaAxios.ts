d736fefb470059d87f294fb899b3b489d27ffa7a
Julio Jimenez <zaor.julio@gmail.com>
2023-02-15 11:12:12 -0500

    pruebas de endpoint

--- app/Infraestructura/Implementacion/Servicios/RepositorioPagosNovafianzaAxios.ts
+++ app/Infraestructura/Implementacion/Servicios/RepositorioPagosNovafianzaAxios.ts
@@ -10,9 +10,11 @@ export class RepositorioPagosNovafianzaHttp implements RepositorioPagosNovafianz
     private readonly HOST = Env.get('URL_PAGOS_NOVAFIANZA')
 
     public async consultarDeuda(tipoDocumento: string, documento: string): Promise<number> {
+        console.log({tipoDocumento, documento});   
         const endpoint = `?pTipoIdCliente=${tipoDocumento}&pIdCliente=${documento}`
-        console.log(endpoint);        
+        console.log({endpoint});        
         const respuesta = await this.clienteHttp.get<RespuestaDeudaNovafianza>(`${this.HOST}${endpoint}`)
+        console.log({respuesta});    
         const IdRetorno = respuesta.RespuestaMetodo.IdRetorno  //Este valor es la deuda en caso de existir o 1.0 en caso de no existir el cliente o 0.0 en caso de no tener deudas
         if(IdRetorno === 1.0) return 0;
         return respuesta.RespuestaMetodo.IdRetorno
