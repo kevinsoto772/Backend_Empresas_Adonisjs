fbe09ab75e0d35dcc42a196347522c4b29872159
Jesid Polo <jesing482@gmail.com>
2023-02-22 10:15:11 -0500

    cambiando nombre de metodo obtener payload jwt

--- providers/AppProvider.ts
+++ providers/AppProvider.ts
@@ -18,7 +18,7 @@ export default class AppProvider {
     // IoC container is ready
     const Request = this.app.container.use('Adonis/Core/Request')
 
-    Request.macro('getJWTPayload', async function () {
+    Request.macro('obtenerPayloadJWT', async function () {
       const authorization = this.header('Authorization')
       const jwt = authorization!.split(' ')[1]
       const payload = await ServicioAutenticacionJWT.obtenerPayload(jwt)
