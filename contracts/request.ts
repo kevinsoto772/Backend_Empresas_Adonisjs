2ee3c0327535269735ca87a58cdb9b966a232e6d
Jesid Polo <jesing482@gmail.com>
2023-02-22 10:03:36 -0500

    cambiando tipo de la macro jwt

--- contracts/request.ts
+++ contracts/request.ts
@@ -2,6 +2,6 @@ import { PayloadJWT } from "App/Dominio/Dto/PayloadJWT";
 
 declare module '@ioc:Adonis/Core/Request' {
     interface RequestContract {
-        getJWTPayload(): PayloadJWT
+        getJWTPayload(): Promise<PayloadJWT>
     }
   }
