0b9011cbeb8329c70f447c254582b4e5df5f95d4
Jesid Polo <jesing482@gmail.com>
2023-03-01 16:06:43 -0500

    endpoints asignacion de servicio

--- app/Dominio/Datos/Entidades/ArchivoEmpresa.ts
+++ app/Dominio/Datos/Entidades/ArchivoEmpresa.ts
@@ -1,10 +1,18 @@
-/* eslint-disable @typescript-eslint/semi */
-/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+import { v4 as uuid } from 'uuid'
 
 export class ArchivoEmpresa{
   id: string;
   idEmpresa: string;
   idArchivo: string;
-  tipo: string;
+  tipo?: string;
   estado: boolean;
+
+  public static crear(idEmpresa: string, idArchivo: string, estado: boolean = true){
+    const archivoEmpresa = new ArchivoEmpresa()
+    archivoEmpresa.id = uuid()
+    archivoEmpresa.idEmpresa = idEmpresa
+    archivoEmpresa.idArchivo = idArchivo
+    archivoEmpresa.estado = estado
+    return archivoEmpresa
+  }
 }
--- start/Rutas/ruta_archivo_empresa.ts
+++ start/Rutas/ruta_archivo_empresa.ts
@@ -3,9 +3,15 @@ import Route from '@ioc:Adonis/Core/Route'
 const accion_path = '../../../app/Presentacion/ArchivoEmpresa/ControladorArchivoEmpresa'
 
 Route.group(() => {
-  Route.post('/registro', accion_path + '.guardarArchivoEmpresa')
+  /* Gets */
+  Route.get('/listar_por_empresa/:idEmpresa', accion_path + '.listarPorEmpresa')
   Route.get('/listar/:pagina?/:limite?', accion_path + '.listar')
   Route.get('/:id', accion_path + '.obtenerArchivoEmpresaPorId')
+  /* Posts */
+  Route.post('/registro', accion_path + '.guardarArchivoEmpresa')
+  Route.post('/registro-multiple', accion_path + '.guardarMultiplesArchivosEmpresa')
+  /* Patchs */
   Route.patch('/:id', accion_path + '.actualizarArchivoEmpresa')
+  /* Puts */
   Route.put('/estado/:id', accion_path + '.cambiarEstado')
 }).prefix('api/v1/archivo_empresa').middleware('autenticacionJwt')
